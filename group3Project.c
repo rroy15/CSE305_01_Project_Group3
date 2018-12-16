051491f018578cf502e71b01329e873e1a86ca93
rroy15 <roy11_rahul@yahoo.com>
2018-12-16 17:58:45 +0600

    Created the File. Set up basic program structure.
    Others to contribute accordingly.

---
+++ group3Project.c
@@ -0,0 +1,54 @@
+/* GROUP 3 
+*  RAHUL ROY ( ID : 172014022)
+*  NICHOLAS TONY GOMES (ID : 172014005)
+*
+*
+*
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+// variable declaration //
+int max, R, C;
+
+
+struct queueElement
+{
+
+
+}qe;
+
+
+
+struct queue
+{
+    
+}q;
+
+
+// QUEUE OPERATIONS  //
+
+
+
+
+// BFS APPLICATION // 
+
+
+
+
+
+
+
+
+// MAIN FUNCTION //
+
+int main ()
+{
+
+
+  return 0 ;
+}
+
+
